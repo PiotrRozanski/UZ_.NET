bab54ba6b5f00bc5e8547154775c945468450060
PiotrRozanski <87191@stud.uz.zgora.pl>
2017-03-14 22:24:12 +0100

    first commit

---
+++ Zegar_Alarm/Zegar_Alarm/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zegar_Alarm
+{
+    public partial class Form1 : Form
+    {
+        Timer timer = new Timer();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer.Interval = 1000;
+            timer.Tick += new EventHandler(this.t_Tick);
+            timer.Start(); 
+        }
+
+        private void t_Tick(object sender, EventArgs e)
+        {
+            int hh = DateTime.Now.Hour;
+            int mm = DateTime.Now.Minute;
+            int ss = DateTime.Now.Second;
+            String actualDate = DateTime.Today.ToString("dd-MM-yyyy");
+
+            string time = "";
+
+            if (hh < 10)
+            {
+                time += "0" + hh;
+            }
+            else
+            {
+                time += hh;
+            }
+            time += ":";
+
+            if (mm < 10)
+            {
+                time += "0" + mm;
+            }
+            else
+            {
+                time += mm;
+            }
+            time += ":";
+
+            if (ss < 10)
+            {
+                time += "0" + ss;
+            }
+            else
+            {
+                time += ss;
+            }
+
+            label1.Text = time + " "+ actualDate;
+        }
+    }
+}
+
