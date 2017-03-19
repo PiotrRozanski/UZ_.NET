c27504bb45c33aa0c4bb65c20b83477082911a68
PiotrRozanski <87191@stud.uz.zgora.pl>
2017-03-19 20:50:02 +0100

    Zegar_alarm Dodanie alarmu do zegara

--- Zegar_Alarm/Zegar_Alarm/Form1.Designer.cs
+++ Zegar_Alarm/Zegar_Alarm/Form1.Designer.cs
@@ -29,6 +29,9 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -37,15 +40,47 @@
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(647, 73);
+            this.label1.Size = new System.Drawing.Size(631, 73);
             this.label1.TabIndex = 0;
             this.label1.Text = "00:00:00 01-01-1999";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(229, 145);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "set alarm";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker1.Location = new System.Drawing.Point(229, 109);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 2;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(331, 145);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(98, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "stop alarm";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(647, 97);
+            this.ClientSize = new System.Drawing.Size(650, 200);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
@@ -56,12 +91,14 @@
             this.ResumeLayout(false);
             this.PerformLayout();
 
-
         }
 
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Zegar_Alarm/Zegar_Alarm/Form1.cs
+++ Zegar_Alarm/Zegar_Alarm/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -13,6 +14,8 @@ namespace Zegar_Alarm
     public partial class Form1 : Form
     {
         Timer timer = new Timer();
+        SoundPlayer player = new SoundPlayer();
+        DateTime alarm = new DateTime();
 
         public Form1()
         {
@@ -23,15 +26,17 @@ namespace Zegar_Alarm
         {
             timer.Interval = 1000;
             timer.Tick += new EventHandler(this.t_Tick);
+            timer.Tick += t_Tick;
             timer.Start(); 
         }
 
         private void t_Tick(object sender, EventArgs e)
         {
+            
+            String actualDate = DateTime.Today.ToString("dd-MM-yyyy");
             int hh = DateTime.Now.Hour;
             int mm = DateTime.Now.Minute;
             int ss = DateTime.Now.Second;
-            String actualDate = DateTime.Today.ToString("dd-MM-yyyy");
 
             string time = "";
 
@@ -64,7 +69,38 @@ namespace Zegar_Alarm
                 time += ss;
             }
 
-            label1.Text = time + " "+ actualDate;
+            label1.Text = time + " " + actualDate;
+
+            button1.Click += new EventHandler(this.button1_Click);
+
+            if (alarm.Hour == hh && alarm.Minute == mm && alarm.Second == ss)
+            {
+                try
+                {
+                    player.SoundLocation = @"C:\Windows\Media\Alarm01.wav";
+                    player.PlayLooping();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            alarm = dateTimePicker1.Value;
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            player.Stop();
         }
     }
 }
