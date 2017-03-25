0588174d00cbe194561ca89c936afe9cbac7cae5
Sebastian Sobierajski <sivik99@onet.eu>
2017-03-25 21:06:40 +0100

    IP Validator dodany

---
+++ IPValidator/IPValidator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPValidator
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Podaj Ip");
+			string ip = Console.ReadLine();
+			var validator = new Validator();
+			validator.ValidateIP(ip);
+			Console.ReadKey();
+			Console.WriteLine("Podaj Email");
+			string email = Console.ReadLine();
+			validator.ValidateMail(email);
+		}
+	}
+}
